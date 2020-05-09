71c49c0674d1a0ad3e4ea541465c4c642a805a02
DESKTOP-09RIGVH\futou <futouyiba@gmail.com>
2020-05-09 21:38:44 +0800

    init

---
+++ src/content/profile.js
@@ -0,0 +1,9 @@
+/* eslint max-len: "off" */
+
+const profile = {
+  title: '个人评价',
+  description: '一个懂开发、会实操、创过业的游戏产品人。曾就职于Kabam & Gameloft，任职主策划/数值策划，项目收入超过5亿美元。理性、注重数据、重思考；富于创造力，且擅长将其落地。持有自研技术专利。',
+  icon: 'fas fa-user',
+};
+
+export default profile;
---
+++ src/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import './styles/styles.css';
+import CV from './components/CV';
+import Data from './content';
+// import Export from './components/Export';
+import store from './store';
+
+ReactDOM.render(
+  <Provider store={store}>
+    <CV {...Data} />
+  </Provider>,
+  global.document.getElementById('root'),
+);
